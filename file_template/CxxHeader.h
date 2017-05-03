bf5b0a51ab0dfe58baa6cb57071986181133ccfb
yuzhangbit <yu.zhang.bit@gmail.com>
2017-05-03 17:29:18 -0400

    add cxx file template

---
+++ file_template/CxxHeader.h
@@ -0,0 +1,16 @@
+#set ($MY_NAME = "Yu Zhang")
+#set ($EMAIL = "yu.zhang.bit@gmail.com")
+#set ($INSTITUTE = "Beijing Institute of Technology")
+#set ($HOME_LAB = "Intelligent Vehicle Research Center(IVRC)")
+#set ($COL_LAB = "Waterloo Autonomous Vehicles Laboratory (WAVELab), University of Waterloo")
+#if ($HEADER_COMMENTS)
+/* Copyright (c) $YEAR, $MY_NAME, $HOME_LAB, 
+ * $INSTITUTE.
+ * $COL_LAB.
+ * All rights reserved.
+ *        Files: ${FILE_NAME}
+ *   Created on: $MONTH, $DAY, $YEAR 
+ *       Author: $MY_NAME
+ *        Email: $EMAIL
+ */
+#end
